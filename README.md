ab0dbf92907336928e906cae225c852c1139c6a2
Astra <105219585+astrabun@users.noreply.github.com>
2024-09-05 22:48:08 +0000

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# SonaSky
+
+> A Bluesky labeler allowing self-assignment of fursona species labels
+
+---
+
+## About
+
+SonaSky is a [vanity](https://en.wikipedia.org/wiki/Vanity_plate) Bluesky labeler (https://bsky.app/profile/sonasky.bsky.social) that allows users to self-assign moderation labels to their ATProto account. Specifically, it was created with the furry fandom in mind. 
+
+Users find a post by the SonaSky account that contains the species name they want to receive as a label, click "like" ❤️, and a bot listening to the Bluesky firehose will see this and apply the appropriate label. This repository covers that bot code.
+
+A web app to browse available labels (because there are a lot of them now) can be found here: https://sonasky-browse.bunnys.ky/
+
+## Setup
+
+Follow the steps for hosting [Ozone](https://github.com/bluesky-social/ozone/blob/main/HOSTING.md), a web interface for labeling content in atproto / Bluesky, to create a labeler. Once created, move on to [Configuration](#configuration).
+
+## Configuration
+
+`$ cp .env.sample .env`, then edit:
+
+```
+BSKY_USER=labelservicehandlegoeshere.bsky.social
+BSKY_PASS=some-app-password-here
+OZONE_SERVICE_USER_DID=did:plc:didofthelabelserviceuserhere
+POSTGRES_HOST=db
+POSTGRES_USER=sona
+POSTGRES_PASSWORD= # generate with: $ openssl rand -hex 32
+POSTGRES_DB=sonabot
+POSTGRES_PORT=5432
+GENERATE_JSON_FILE_OF_SPECIES=false  # if true, will produce a file at /tmp/labels/labels.json
+```
+
+## Usage
+
+```
+$ docker compose up -d
+```
+
+As the labeler account, create one or more posts that start with `Species: ` &nbsp;or `Meta: `&nbsp;; Avoid using special characters as label IDs (at least in my experience) must be lowercase alphanumeric. Once a species label is liked on Bluesky, the bot will see this in the Firehose and 1) create the label with a default display name and description in English, then 2) assign the user that liked the post that label, then 3) add a record to a postgres database with the user's DID, the operation's CID, and the label ID that was assigned to them. The contents of this DB are dual purpose. First and most important, it makes it easy to handle users who want to *remove* a label from their account. Since "unlike" events on the firehose don't contain the DID of the user who performed the action, we must go on the operation's CID value instead, and if we keep that in the DB, we can look for the matching CID and label ID, then remove that from the user in that DB record. Secondly, we can use this DB for something like generating statistics on how many users are using each species label. This isn't nearly as important as the option to opt-out, but it is nice to have.
+
+While on the subject of the DB, it's also used to store checkpoints on where the bot is in the Bluesky firehose. If there is a power failure or network connection interruption, the bot will have a cursor location to use when resuming. In that scenario, the bot will run behind real time until it manages to catch up to the present. The DB also stores the Bluesky post IDs for each label which is used to make navigation to posts easier from the SonaSky-browse web app. 
+
+## Localization
+
+To contribute to localization of SonaSky labels, see [sonasky-labels-localization](https://github.com/astrabun/sonasky-labels-localization). Labels are added in English by default, but I would love to be inclusive to others and localize to other languages :)
+
+## Credits
+
+SonaSky uses [bluesky-social/ozone](https://github.com/bluesky-social/ozone) to run the labeler itself; I know there are alternatives like `@skyware/labeler` but I kinda like the Ozone UI. Maybe I'll change how I want to run this in the future, but for now, I'm sticking with Ozone. Also, it's a cool name.
+
+Shoutout to [notjuliet/@adorable.mom](https://github.com/notjuliet) for creating [notjuliet/pronouns-bsky](https://github.com/notjuliet/pronouns-bsky) which in part inspired me to create SonaSky! Also, without her pronouns labeler code I wouldn't have known about skyware's firehose client which has helped SonaSky stability greatly.
---
+++ src/index.ts
@@ -0,0 +1,501 @@
+import { Firehose, RepoOp } from "@skyware/firehose";
+
+import { BskyAgent, AppBskyActorDefs, AppBskyActorProfile } from "@atproto/api";
+
+import * as fs from "fs";
+
+import { Client as PgClient } from "pg";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+const GENERATE_JSON_FILE_OF_SPECIES =
+  process.env.GENERATE_JSON_FILE_OF_SPECIES === "true";
+const GENERATE_JSON_FILE_OF_SPECIES__DIR = "/tmp/labels";
+const GENERATE_JSON_FILE_OF_SPECIES__NAME = "labels.json";
+
+const ozone_service_user_did = process.env.OZONE_SERVICE_USER_DID as string;
+
+const dbclient = new PgClient({
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: process.env.POSTGRES_PORT
+    ? parseInt(process.env.POSTGRES_PORT as string)
+    : 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+const agent = new BskyAgent({
+  service: "https://bsky.social",
+});
+
+BskyAgent.configure({
+  appLabelers: [process.env.OZONE_SERVICE_USER_DID ?? ""],
+});
+
+function toTitleCase(str: string) {
+  return str.replace(
+    /\w\S*/g,
+    (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+  );
+}
+
+async function main() {
+  await agent.login({
+    identifier: process.env.BSKY_USER as string,
+    password: process.env.BSKY_PASS as string,
+  });
+
+  const getOzoneCurrentPolicies = async () => {
+    await agent.refreshSession();
+    let response = await agent
+      .withProxy("atproto_labeler", ozone_service_user_did)
+      .api.com.atproto.repo.getRecord({
+        repo: ozone_service_user_did,
+        collection: "app.bsky.labeler.service",
+        rkey: "self",
+      });
+    if (GENERATE_JSON_FILE_OF_SPECIES) {
+      if (!fs.existsSync(GENERATE_JSON_FILE_OF_SPECIES__DIR)) {
+        fs.mkdirSync(GENERATE_JSON_FILE_OF_SPECIES__DIR, { recursive: true });
+      }
+      const knownSonaPosts = await dbclient.query(`
+                SELECT postid, species FROM sonaposts
+            `);
+      const current_simple = (
+        response as any
+      ).data.value.policies.labelValueDefinitions.map((i: any) => {
+        let postsLink = `https://bsky.app/search?q=from%3A${
+          process.env.BSKY_USER
+        }+%22species%3A%22+%22${i.identifier.replace(/\-/g, "+")}%22`;
+        let sonaPostDefined =
+          knownSonaPosts.rows.filter((j) => j.species === i.identifier)
+            .length === 1;
+        let sonaPost;
+        if (sonaPostDefined) {
+          sonaPost = `https://bsky.app/profile/${ozone_service_user_did}/post/${
+            knownSonaPosts.rows.filter((j) => j.species === i.identifier)[0]
+              .postid
+          }`;
+        }
+        return {
+          id: i.identifier,
+          name: i.locales[0].name,
+          description: i.locales[0].description,
+          locales: i.locales,
+          posts: sonaPost || postsLink,
+        };
+      });
+      fs.writeFile(
+        `${GENERATE_JSON_FILE_OF_SPECIES__DIR}/${GENERATE_JSON_FILE_OF_SPECIES__NAME}`,
+        JSON.stringify(current_simple),
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    }
+    return (response as any).data.value.policies;
+  };
+
+  const addLabelIfNotInOzoneCurrentPolicies = async (
+    label: string,
+    safeLabel?: string,
+    postId?: string,
+    isMetaTag?: boolean
+  ) => {
+    await agent.refreshSession();
+    let current_policies = await getOzoneCurrentPolicies();
+    let name = toTitleCase(label.replace(/-/g, " "));
+    const knownSonaPosts = await dbclient.query(
+      `
+            SELECT postid, species FROM sonaposts
+            WHERE species = $1
+            LIMIT 1;
+        `,
+      [label]
+    );
+    if (knownSonaPosts.rowCount === 0) {
+      dbclient
+        .query(
+          `
+                insert into sonaposts (postid,species) VALUES ($1, $2) RETURNING *;
+            `,
+          [postId || "", label]
+        )
+        .then((res) => {
+          console.log(`Logged to checkpoint table: ${JSON.stringify(res)}`);
+        })
+        .catch((err) => console.error(err));
+    }
+    if (
+      current_policies.labelValues.filter((i: string) => i === safeLabel)
+        .length === 0
+    ) {
+      let labelValues = current_policies.labelValues;
+      let labelValueDefinitions = current_policies.labelValueDefinitions;
+
+      labelValues.push(safeLabel);
+      labelValueDefinitions.push({
+        blurs: "none",
+        locales: [
+          {
+            lang: "en",
+            name: name,
+            description:
+              isMetaTag === true
+                ? `${name} [Category: Meta]`
+                : `This user is a${
+                    ["a", "e", "i", "o", "u"].includes(
+                      name.at(0)!.toLowerCase()
+                    )
+                      ? "n"
+                      : ""
+                  } ${name}!`,
+          },
+        ],
+        severity: "inform",
+        adultOnly: false,
+        identifier: safeLabel ? safeLabel : label,
+        defaultSetting: "warn",
+      });
+      let body = {
+        repo: ozone_service_user_did,
+        collection: "app.bsky.labeler.service",
+        rkey: "self",
+        record: {
+          $type: "app.bsky.labeler.service",
+          policies: {
+            labelValues: labelValues,
+            labelValueDefinitions: labelValueDefinitions,
+          },
+          createdAt: new Date().toISOString(),
+        },
+      };
+      let response = await agent
+        .withProxy("atproto_labeler", ozone_service_user_did)
+        .api.com.atproto.repo.putRecord(body);
+      console.log("added new label: ", label, `[${response.success}]`);
+    } else {
+      console.log("Label already exists, didn't have to add a new one.");
+    }
+  };
+
+  const updateDidRecordOzone = async (
+    subject: string | AppBskyActorDefs.ProfileView,
+    _cid: string,
+    action: "add" | "remove",
+    labelVal: string
+  ) => {
+    await agent.refreshSession();
+    let labeller_did = agent.session?.did;
+    const did = AppBskyActorDefs.isProfileView(subject) ? subject.did : subject;
+    const repo = await agent
+      .withProxy("atproto_labeler", labeller_did!)
+      .api.tools.ozone.moderation.getRepo({ did: did })
+      .catch((err) => {
+        console.log(err);
+      });
+    if (!repo) return;
+    if (action === "remove") {
+      await agent
+        .withProxy("atproto_labeler", labeller_did!)
+        .api.tools.ozone.moderation.emitEvent({
+          event: {
+            $type: "tools.ozone.moderation.defs#modEventLabel",
+            createLabelVals: [],
+            negateLabelVals: [labelVal],
+          },
+          subject: {
+            $type: "com.atproto.admin.defs#repoRef",
+            did: did,
+          },
+          createdBy: agent.session!.did,
+          createdAt: new Date().toISOString(),
+          subjectBlobCids: [],
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .then(() => console.log(`Deleted label ${labelVal} for ${did}`));
+    } else {
+      await agent
+        .withProxy("atproto_labeler", labeller_did!)
+        .api.tools.ozone.moderation.emitEvent({
+          event: {
+            $type: "tools.ozone.moderation.defs#modEventLabel",
+            createLabelVals: [labelVal],
+            negateLabelVals: [],
+          },
+          subject: {
+            $type: "com.atproto.admin.defs#repoRef",
+            did: did,
+          },
+          createdBy: agent.session!.did,
+          createdAt: new Date().toISOString(),
+          subjectBlobCids: [],
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .then(() => console.log(`Labeled ${did} with ${labelVal}`));
+    }
+  };
+
+  await dbclient.connect();
+  await dbclient.query(`
+    CREATE TABLE IF NOT EXISTS sonas (
+        id SERIAL PRIMARY KEY,
+        did VARCHAR(255) NOT NULL,
+        likepath VARCHAR(255) NOT NULL,
+        posturi VARCHAR(255) NOT NULL,
+        species VARCHAR(255) NOT NULL,
+        ts TIMESTAMP NOT NULL
+    );
+    `);
+
+  await dbclient.query(`
+    CREATE TABLE IF NOT EXISTS sonaposts (
+        id SERIAL PRIMARY KEY,
+        postid VARCHAR(255) NOT NULL,
+        species VARCHAR(255) NOT NULL
+    );
+    `);
+
+  await dbclient.query(`
+    CREATE TABLE IF NOT EXISTS firehosecheckpoint (
+        id SERIAL PRIMARY KEY,
+        cursor VARCHAR(255) NOT NULL,
+        ts TIMESTAMP NOT NULL
+    );
+    `);
+
+  let cursorFirehose = 0;
+  let cursorFirehoseTs = "";
+
+  // Cycles through 0-9, and any time it hits 0, update display name with approximate label delay.
+  // In effect, every 10 minutes, the display name will reflect how far behind the labeler is
+  let updateDisplayNameTicker = 0;
+
+  // get last known cursor if exists
+  const lastKnownCheckpoint = await dbclient.query(`
+        SELECT ts, cursor FROM firehosecheckpoint
+        ORDER BY ts DESC
+        LIMIT 1;
+    `);
+  let lastCursor;
+  const lastKnownCheckpointObj = lastKnownCheckpoint.rows[0] || null;
+  if (lastKnownCheckpointObj !== null) {
+    let lastTs = lastKnownCheckpointObj.ts;
+    lastCursor = lastKnownCheckpointObj.cursor;
+    console.log(`Last checkpoint added at ${lastTs} at cursor: ${lastCursor}`);
+  } else {
+    console.log("No known last checkpoint, just gonna... start.");
+  }
+
+  const firehose = new Firehose({ cursor: lastCursor ?? "" });
+
+  firehose.on("error", ({ cursor, error }) => {
+    console.log(`Firehose errored on cursor: ${cursor}`, error);
+    throw new Error("EXITING DUE TO FIREHOSE ERROR");
+  });
+
+  firehose.on("open", () => {
+    const updateDisplayName = async () => {
+      // I would prefer to use agent.upsertProfile(), but this consistently throws an error: Record/avatar should be a blob ref
+      // so I'm just re-doing this myself
+      // First get current profile info:
+      if (!agent.session) {
+        throw new Error("Not logged in");
+      }
+      const existing = await agent.com.atproto.repo
+        .getRecord({
+          repo: agent.session.did,
+          collection: "app.bsky.actor.profile",
+          rkey: "self",
+        })
+        .catch((_) => undefined);
+      if (!existing) {
+        throw new Error("No profile somehow");
+      }
+      const newRecord: AppBskyActorProfile.Record = { ...existing.data.value };
+      // newRecord.displayName = `SonaSky [Currently @${cursorFirehoseTs}|Delay~= ${dayjs(cursorFirehoseTs).fromNow(true)}]`;
+      let newDisplayName = "SonaSky";
+      // only show the delay in the display name if more than 5 minutes
+      if (dayjs().diff(dayjs(cursorFirehoseTs), "minute") > 5) {
+        newDisplayName = `SonaSky [Delayed by ~${dayjs(
+          cursorFirehoseTs
+        ).fromNow(true)}]`;
+      }
+      newRecord.displayName = newDisplayName;
+      //             newRecord.description = `Show off your fursona (label)!
+      // ❓: Find a species post, and like it! To remove, unlike it.
+      // 🔍: Browse/Species Request Instructions: https://sonasky-browse.bunnys.ky/ <-- PLEASE READ/POR FAVOR LEIA
+
+      // 🐇🧑‍💻: @astra.bunnys.ky <- 18+
+      // 🖼️: @snowfox.gay <- 18+`;
+      newRecord.description = `Show off your fursona (label)!
+❓: Add=❤️! Remove=💔.
+🔍: Browse/Requests: https://sonasky-browse.bunnys.ky/ <-- READ/LEIA
+
+🐇🧑‍💻: @astra.bunnys.ky <- 18+
+🖼️: @snowfox.gay <- 18+
+Cursor @ ${cursorFirehoseTs.split(".")[0]}Z, Delays ~= ${dayjs(
+        cursorFirehoseTs
+      ).fromNow(true)}`;
+      await agent.com.atproto.repo.putRecord({
+        repo: agent.session.did,
+        collection: "app.bsky.actor.profile",
+        rkey: "self",
+        record: newRecord,
+        swapRecord: existing?.data.cid || null,
+      });
+    };
+    updateDisplayName();
+    updateDisplayNameTicker = (updateDisplayNameTicker + 1) % 10; // update ticker;
+
+    setInterval(() => {
+      const timestamp = new Date().toISOString();
+      console.log(
+        `${timestamp} cursor: ${cursorFirehose} (which is ~${cursorFirehoseTs})`
+      );
+      getOzoneCurrentPolicies(); // trigger dump to file if enabled
+      try {
+        if (!fs.existsSync(GENERATE_JSON_FILE_OF_SPECIES__DIR)) {
+          fs.mkdirSync(GENERATE_JSON_FILE_OF_SPECIES__DIR, { recursive: true });
+        }
+        fs.writeFile(
+          `${GENERATE_JSON_FILE_OF_SPECIES__DIR}/cursortime.txt`,
+          cursorFirehoseTs,
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      } catch {}
+      dbclient
+        .query(
+          `
+                insert into firehosecheckpoint (cursor,ts) VALUES ($1, current_timestamp) RETURNING *;
+            `,
+          [cursorFirehose.toString()]
+        )
+        .then((res) => {
+          console.log(`Logged to checkpoint table: ${JSON.stringify(res)}`);
+        })
+        .catch((err) => console.error(err));
+
+      // Delay ticker logic
+      if (updateDisplayNameTicker === 0) {
+        updateDisplayName();
+      }
+      updateDisplayNameTicker = (updateDisplayNameTicker + 1) % 10; // update ticker;
+    }, 60000); // Every minute, store checkpoint in db; may raise this in the future.
+  });
+
+  firehose.on("commit", async (commit) => {
+    cursorFirehose = commit.seq;
+    cursorFirehoseTs = commit.time;
+    commit.ops.forEach(async (op: RepoOp) => {
+      try {
+        if (op.path.includes(".like/")) {
+          const dbrow: any = {
+            did: commit.repo,
+            ts: commit.time,
+            likepath: op.path,
+          };
+          if (op.action === "create") {
+            if (!JSON.stringify(op).includes(ozone_service_user_did)) {
+              return;
+            }
+            dbrow["posturi"] = ((op as any).record.subject as any).uri;
+            let p = await agent.getPost({
+              repo: `${dbrow.posturi.replace("at://", "").split("/")[0]}`,
+              rkey: `${dbrow.posturi.split("/").slice(-1).join("/")}`,
+            });
+            let post_role_text = p.value.text;
+            if (
+              !post_role_text.startsWith("Species: ") &&
+              !post_role_text.startsWith("Meta: ")
+            ) {
+              return;
+            }
+            let species = post_role_text
+              .replace("Species: ", "")
+              .replace("Meta: ", "")
+              .split("//")[0]
+              .trim()
+              .replace(/ /g, "-")
+              .toLowerCase();
+            let safe_species = species.replace("'", "");
+            addLabelIfNotInOzoneCurrentPolicies(
+              species,
+              safe_species,
+              `${dbrow.posturi.split("/").slice(-1).join("/")}`,
+              post_role_text.startsWith("Meta: ")
+            );
+            const insertResult = await dbclient.query(
+              `
+                            INSERT INTO sonas (did, likepath, posturi, species, ts) VALUES ($1, $2, $3, $4, $5) RETURNING *;
+                          `,
+              [dbrow.did, dbrow.likepath, dbrow.posturi, safe_species, dbrow.ts]
+            );
+            console.log("Inserted:", insertResult.rows[0]);
+            await updateDidRecordOzone(dbrow.did, "n/a", "add", safe_species);
+          } else {
+            // removed
+            try {
+              const selectResult = await dbclient.query(
+                `
+                                SELECT * FROM sonas WHERE likepath = $1 AND did = $2;
+                              `,
+                [dbrow.likepath, dbrow.did]
+              );
+              if (selectResult.rows.length > 0) {
+                let species_vals = [
+                  ...new Set(selectResult.rows.map((i) => i.species)),
+                ];
+                const deleteResult = await dbclient.query(
+                  `
+                                    DELETE FROM sonas WHERE likepath = $1 AND did = $2 RETURNING *;
+                                  `,
+                  [dbrow.likepath, dbrow.did]
+                );
+                console.log("Deleted:", deleteResult.rows[0]);
+                species_vals.forEach(async (species_val) => {
+                  console.log(species_val);
+                  await updateDidRecordOzone(
+                    dbrow.did,
+                    "",
+                    "remove",
+                    species_val
+                  );
+                  const deleteResult = await dbclient.query(
+                    `
+                                        DELETE FROM sonas WHERE species = $1 AND did = $2 RETURNING *;
+                                      `,
+                    [species_val, dbrow.did]
+                  );
+                  console.log("Deleted:", deleteResult.rows[0]);
+                });
+              }
+            } catch (err) {
+              console.error(err);
+            }
+          }
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    });
+  });
+
+  firehose.start();
+}
+
+main().catch((err) => {
+  throw Error(`Need to restart... ${err}`);
+});
