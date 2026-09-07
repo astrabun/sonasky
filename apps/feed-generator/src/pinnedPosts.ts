a0d1766bfbb91dab11e495876eb72195b793de56
Astra <105219585+astrabun@users.noreply.github.com>
2026-09-07 16:16:23 -0400

    one more pin

--- apps/feed-generator/src/pinnedPosts.ts
+++ apps/feed-generator/src/pinnedPosts.ts
@@ -32,6 +32,11 @@ type FeedKind = "all" | "species" | "trending" | "interacted";
 
 export const pinnedPosts: PinnedPost[] = [
   {
+    uri: "at://did:plc:2qawvcwumvgxmed6iy6pmt6l/app.bsky.feed.post/3muu4guygkc23",
+    feeds: ["interacted", "trending", "all"],
+    position: 0,
+  },
+  {
     uri: "at://did:plc:nkleu4mgtlxpsfwkdm6otsqu/app.bsky.feed.post/3mutboyzfcc2q",
     feeds: ["*"],
     position: 10,
